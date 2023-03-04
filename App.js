32984d39ecff60de01c1586702bba39c83414a2c
Kalerdiksha <100907581+Kalerdiksha@users.noreply.github.com>
2023-03-04 23:31:06 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Stock from './Stock';
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <stock></stock>
+    </div>
+  );
+}
+
+export default App;
